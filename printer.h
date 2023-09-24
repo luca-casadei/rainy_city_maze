d36c5bdc881b13f592f913b9eef06ab5118ea81d
Luca Casadei <casadeiluca30@gmail.com>
2023-09-24 17:55:42 +0200

    Removed leftover files

--- printer.h
+++
@@ -1,4 +0,0 @@
-/*Stampa a video sia il numero di celle percorse che il percorso seguito.*/
-void print_path(int source, int destination,
-	Map map, Distance* d, int* p);
-void print_map_graphics(Map map);
--- utility.h
+++
@@ -1,3 +0,0 @@
-/*Converte da char a int*/
-#define TOINT(character) ((character) - '0')
-#define NODE_UNDEF -1
